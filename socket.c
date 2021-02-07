2b7b52e65394849faa3a274b50f98d0c6458a7f6
Björn Åström <b@strm.space>
2021-02-07 11:34:14 +0100

    Add SSL timeout

--- socket.c
+++ socket.c
@@ -1,7 +1,10 @@
 #include <wolfssl/ssl.h>
 #include <fcntl.h>
+#include <sys/time.h>
 #include "probeably.h"
 #include "socket.h"
+
+#define TIMEOUT 3
 
 char err_buf[80];// TODO: use MAX_ERROR_SZ
 
@@ -18,15 +21,17 @@ void prb_socket_cleanup()
 static int connect_raw(struct prb_socket *s, char *ip, int port)
 {
 	int sock;
+	int err;
 	PRB_DEBUG("connect", "Connecting raw socket\n");
 	s->sock = socket(AF_INET, SOCK_STREAM, 0);
-	fcntl(s->sock, F_SETFL, fcntl(s->sock, F_GETFL, 0), O_NONBLOCK);
+	fcntl(s->sock, F_SETFL, fcntl(s->sock, F_GETFL, 0) | O_NONBLOCK);
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port);
 	inet_pton(AF_INET, ip, &addr.sin_addr);
 
-	if (connect(s->sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+	while (( err = connect(s->sock, (struct sockaddr *) &addr, sizeof(addr))) & EINPROGRESS & EALREADY & EAGAIN);
+	if (err < 0) {
 		perror("connect");
 		return -1;
 	}
@@ -43,8 +48,7 @@ int prb_socket_connect(struct prb_socket *s, char *ip, int port)
 
 	int err;
 	WOLFSSL_CTX* ctx;
-	wolfSSL_CTX_set_timeout(ctx, 3);
-
+	wolfSSL_CTX_set_timeout(ctx, TIMEOUT);
 
 	if (s->type == PRB_SOCKET_SSL || s->type == PRB_SOCKET_UNKNOWN) {
 		PRB_DEBUG("connect", "Attempting to perform SSL handshake\n");
@@ -61,11 +65,18 @@ int prb_socket_connect(struct prb_socket *s, char *ip, int port)
 		}
 
 		wolfSSL_set_using_nonblock(s->ssl, 1);
-		wolfSSL_set_timeout(s->ssl, 3);
+		wolfSSL_set_timeout(s->ssl, TIMEOUT);
 
 		wolfSSL_set_fd(s->ssl, s->sock);
 
-		err = wolfSSL_connect(s->ssl);
+		struct timeval start_time, end_time;
+		gettimeofday(&start_time, NULL);
+		while ((err = wolfSSL_connect(s->ssl)) & SSL_ERROR_WANT_READ & SSL_ERROR_WANT_WRITE) {
+			gettimeofday(&end_time, NULL);
+			if ((end_time.tv_sec - start_time.tv_sec) > TIMEOUT) {
+				break;
+			}
+		}
 		if (err != SSL_SUCCESS) {
 			PRB_DEBUG("connect", "SSL handshake failed\n");
 			printf("failed connecting to SSL server: %d: %s\n", wolfSSL_get_error(s->ssl, err), wolfSSL_ERR_error_string(wolfSSL_get_error(s->ssl, err), err_buf));
@@ -90,7 +101,6 @@ void prb_socket_close(struct prb_socket *s)
 			wolfSSL_shutdown(s->ssl);
 			//wolfSSL_CTX_free(ctx);
 
-			/* fallthrough */
 		case PRB_SOCKET_RAW:
 			//shutdown(s->sock);
 			break;
