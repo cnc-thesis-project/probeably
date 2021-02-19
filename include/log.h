c71d160df48cbb0385fd0fbec338f1bed637163f
pedroingvar <0x696b7559@gmail.com>
2021-02-19 16:12:50 +0100

    Fix info

--- include/log.h
+++ include/log.h
@@ -7,7 +7,7 @@
 // `do { ... } while (0)` is necessary to use multiple statement in macro,
 // otherwise it can mess up stuff like single if-statement.
 #define PRB_DEBUG(LABEL, ARGS...) do { print_hash_color(WORKER_ID); printf("[%d-%s-debug]\033[0m ", WORKER_ID, LABEL); printf(ARGS); printf("\n"); } while(0)
-#define PRB_INFO(LABEL, ARGS...) do { print_hash_color(WORKER_ID); printf("[%d-%s-warn]\033[0m ", WORKER_ID, LABEL); printf(ARGS); } while(0)
+#define PRB_INFO(LABEL, ARGS...) do { print_hash_color(WORKER_ID); printf("[%d-%s-info]\033[0m ", WORKER_ID, LABEL); printf(ARGS); } while(0)
 #define PRB_ERROR(LABEL, ARGS...) do { print_hash_color(WORKER_ID); printf("[%d-%s-error]\033[0m \033[41m", WORKER_ID, LABEL); printf(ARGS); printf("\033[0m\n"); } while (0)
 
 void print_hash_color(int num);
