8fcc0d2f46b1ac4e41ee2f7cd12b1a425a002d59
Björn Åström <bjoast@gmail.com>
2021-03-09 15:15:59 +0100

    Add telnet prober

--- include/module-geoip.h
+++ include/module-geoip.h
@@ -1,5 +1,5 @@
-#ifndef _PROBEABLE_MODULE_GEOIP_H
-#define _PROBEABLE_MODULE_GEOIP_H
+#ifndef _PROBEABLY_MODULE_GEOIP_H
+#define _PROBEABLY_MODULE_GEOIP_H
 
 extern struct prb_module module_geoip;
 
