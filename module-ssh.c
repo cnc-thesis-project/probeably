9b1575e5398d7bc62a4cc14d5242735f74ef608a
pedroingvar <0x696b7559@gmail.com>
2021-03-30 04:31:37 +0200

    Get dsa key

--- module-ssh.c
+++ module-ssh.c
@@ -11,6 +11,10 @@ static char ssh_buffer[SSH_BUFFER_SIZE];
 
 #define SSH_BANNER "SSH-2.0-OpenSSH_7.9 FreeBSD-20200214\r\n"
 
+static int has_ecdsa = 0;
+static int has_ed25519 = 0;
+static int has_dsa = 0;
+
 static const char cipher_exchange_ecdsa[] =
 	// packet length
 	"\x00\x00\x04\x54"
@@ -179,6 +183,62 @@ static const char cipher_exchange_ed25519[] =
 	// ssh padding
 	"\x00\x00\x00\x00\x00\x00\x00\x00"; // intentionally one null characeter less
 
+static const char cipher_exchange_dsa[] =
+	// packet length
+	"\x00\x00\x04\x1c"
+	// ssh padding length
+	"\x05"
+	// key exchange init message
+	"\x14"
+	// cookie (what?)
+	"\x82\x22\x65\xcd\x54\x16\x6f\x62\x7b\xef\x2f\x8a\xb8\xbf\xcc\x08"
+	// kex algorithms length
+	"\x00\x00\x01\x25"
+	// kex alggoriths string
+	"curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha256,diffie-hellman-group14-sha1"
+	// server host key algorithm length
+	"\x00\x00\x00\x07"
+	// server host key algorithm string
+	"ssh-dss"
+	// encryption algorithms client to server length
+	"\x00\x00\x00\x6c"
+	// encryption algorithms client to server string
+	"chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com"
+	// encryption algorithms server to client length
+	"\x00\x00\x00\x6c"
+	// encryption algorithms server to client string
+	"chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com"
+	// mac algorithms client to server length
+	"\x00\x00\x00\xd5"
+	// mac algorithms client to server string
+	"umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1"
+	// mac algorithms server to client length
+	"\x00\x00\x00\xd5"
+	// mac algorithms server to client string
+	"umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1"
+	// compression algorithms client to server length
+	"\x00\x00\x00\x15"
+	// compression algorithms client to server string
+	"none,zlib@openssh.com"
+	// compression algorithms server to client length
+	"\x00\x00\x00\x15"
+	// compression algorithms server to client string
+	"none,zlib@openssh.com"
+	// languages client to server length
+	"\x00\x00\x00\x00"
+	// languages client to server string (empty)
+	""
+	// languages server to client length
+	"\x00\x00\x00\x00"
+	// languages server to client string (empty)
+	""
+	// first kex packet follows
+	"\x00"
+	// reserved
+	"\x00\x00\x00\x00"
+	// ssh padding
+	"\x00\x00\x00\x00"; // intentionally one null characeter less
+
 static const char key_exchange[48] =
 	// ssh packet length
 	"\x00\x00\x00\x2c"
@@ -283,25 +343,44 @@ static void ssh_probe(struct probeably *p, struct prb_request *r, struct prb_soc
 		PRB_DEBUG("ssh", "Ciphers packet of size %d already received, presumably...", ciphers_len);
 	}
 
-	if (!strcmp(key_type, "ssh-rsa")) // store cipher list only once, what the key type is doesn't matter
+	if (!strcmp(key_type, "ssh-rsa")) {
+		// store cipher list only once
 		prb_write_data(p, r, "ssh", "ciphers", ciphers, ciphers_len, PRB_DB_SUCCESS);
+		// rough way of checking available server host key algorithm
+		if (memmem(ciphers, ciphers_len, "ecdsa-sha2-nistp", strlen("ecdsa-sha2-nistp")))
+			has_ecdsa = 1;
+		if (memmem(ciphers, ciphers_len, "ssh-ed25519", strlen("ssh-ed25519")))
+			has_ed25519 = 1;
+		if (memmem(ciphers, ciphers_len, "ssh-dss", strlen("ssh-dss")))
+			has_dsa = 1;
+	}
 
 	// resend servers cipher list
 	prb_socket_write(s, kex_probe, kex_probe_size);
 
+	PRB_DEBUG("ssh", "Reading public key: %s", key_type);
 	// key exchange, pretty useless but necessary to get public key from the server
 	prb_socket_write(s, key_exchange, sizeof(key_exchange));
 	read_len = prb_socket_read(s, ssh_buffer, SSH_BUFFER_SIZE);
 	prb_socket_shutdown(s);
 
-	prb_write_data(p, r, "ssh", key_type, ssh_buffer, read_len, PRB_DB_SUCCESS);
+	if (read_len > 0)
+		prb_write_data(p, r, "ssh", key_type, ssh_buffer, read_len, PRB_DB_SUCCESS);
 }
 
 static int ssh_module_run(struct probeably *p, struct prb_request *r, struct prb_socket *s)
 {
+	has_ecdsa = 0;
+	has_ed25519 = 0;
+	has_dsa = 0;
+
 	ssh_probe(p, r, s, cipher_exchange_rsa, sizeof(cipher_exchange_rsa), "ssh-rsa");
-	ssh_probe(p, r, s, cipher_exchange_ecdsa, sizeof(cipher_exchange_ecdsa), "ssh-ecdsa");
-	ssh_probe(p, r, s, cipher_exchange_ed25519, sizeof(cipher_exchange_ed25519), "ssh-ed25519");
+	if (has_ecdsa)
+		ssh_probe(p, r, s, cipher_exchange_ecdsa, sizeof(cipher_exchange_ecdsa), "ssh-ecdsa");
+	if (has_ed25519)
+		ssh_probe(p, r, s, cipher_exchange_ed25519, sizeof(cipher_exchange_ed25519), "ssh-ed25519");
+	if (has_dsa)
+		ssh_probe(p, r, s, cipher_exchange_dsa, sizeof(cipher_exchange_dsa), "ssh-dsa");
 
 	return 0;
 }
